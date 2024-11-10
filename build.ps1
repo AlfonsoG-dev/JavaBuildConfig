16a7d554ce17069f332da9d9ae2317999ae26e54
AlfonsoG-dev <alfgomachi@gmail.com>
2024-11-09 19:00:45 -0500

    build: add other directory

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$srcClases = ".\src\*.java "
+$srcClases = ".\src\*.java .\src\Application\Utils\*.java "
 $libFiles = ""
 $compile = "javac -Werror -Xlint:all -d .\bin\ $srcClases"
 $createJar = "jar -cfm JavaBuildConfig.jar Manifesto.txt -C .\bin\ ."
