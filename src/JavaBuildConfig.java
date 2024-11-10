8469c64e536876bfd425f9b4dee303bb794e507d
AlfonsoG-dev <alfgomachi@gmail.com>
2024-11-10 18:01:57 -0500

    docs: add cli options

--- src/JavaBuildConfig.java
+++ src/JavaBuildConfig.java
@@ -2,7 +2,24 @@ import Application.Operations.CommandOperations;
 class JavaBuildConfig {
     public static void main(String[] args) {
         CommandOperations cp = new CommandOperations("./");
-        String c = cp.compile();
-        System.out.println(c);
+        for(int i=0; i<args.length; ++i) {
+            switch(args[i]) {
+                case "--compile":
+                    System.out.println("[cmd] " + cp.compile());
+                    break;
+                case "--h":
+                    messages();
+                    break;
+                case "--help":
+                    messages();
+                    break;
+                default:
+                    System.out.println("[Info] use --h or help to show help");
+                    break;
+            }
+        }
+    }
+    private static void messages() {
+        System.out.println("[Info] use --compile to compile the project");
     }
 }
