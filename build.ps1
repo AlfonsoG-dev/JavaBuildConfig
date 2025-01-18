4612ec6d209cc8710e9a3cb182da88820d818fe5
AlfonsoG-dev <alfgomachi@gmail.com>
2025-01-18 14:47:04 -0500

    refactor: extension for build script of ps1 on windows

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$srcClases = ".\src\*.java .\src\Application\Operations\*.java .\src\Application\Utils\*.java .\src\Application\*.java "
+$srcClases = ".\src\*.java .\src\Application\Operations\*.java .\src\Application\*.java .\src\Application\Utils\*.java "
 $libFiles = ""
 $compile = "javac -Werror -Xlint:all -d .\bin\ $srcClases"
 $createJar = "jar -cfm JavaBuildConfig.jar Manifesto.txt -C .\bin\ ."
