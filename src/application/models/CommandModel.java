530ad9da907596f9a0db7611ba43f9f9c1a76353
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-02 23:50:58 -0500

    refactor: use interface model to de-couple reusable code

---
+++ src/application/models/CommandModel.java
@@ -0,0 +1,45 @@
+package application.models;
+
+import application.operation.FileOperation;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import java.io.File;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public interface CommandModel {
+
+    public FileOperation getFileOperation();
+    public String getCommand(String targetURL, String flags, boolean includeLib);
+
+    public default String getLibURL() {
+        return Paths.get("lib").toString();
+    }
+
+    public default StringBuilder prepareLibFiles() {
+        StringBuilder libs = new StringBuilder();
+        libs.append(getFileOperation().libFiles(getLibURL())
+            .stream()
+            .map(Path::toString)
+            .collect(Collectors.joining(";"))
+        );
+        return libs;
+    }
+    /**
+     * join files path as src\directory\*.java
+     */
+    public default String prepareSrcFiles() {
+        StringBuilder src = new StringBuilder();
+        src.append(getFileOperation().sourceDirs()
+                .stream()
+                .map(Path::toString)
+                .collect(Collectors.joining(File.separator + "*.java "))
+        );
+        // with collectors joining the last line doesn't have the aggregation \*.java
+        String clean = src.toString() + File.separator + "*.java";
+        return clean;
+    }
+}
--- src/application/models/CompileModel.java
+++ src/application/models/CompileModel.java
@@ -10,36 +10,13 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public record CompileModel(String root, FileOperation op) {
+public record CompileModel(String root, FileOperation op) implements CommandModel {
 
-    private String getLibURL() {
-        return Paths.get(root).resolve("lib").toString();
+    @Override
+    public FileOperation getFileOperation() {
+        return op;
     }
-
-    private StringBuilder prepareLibFiles() {
-        StringBuilder libs = new StringBuilder();
-        libs.append(op.libFiles(getLibURL())
-            .stream()
-            .map(Path::toString)
-            .collect(Collectors.joining(";"))
-        );
-        return libs;
-    }
-    /**
-     * join files path as src\directory\*.java
-     */
-    private String prepareSrcFiles() {
-        StringBuilder src = new StringBuilder();
-        src.append(op.sourceDirs()
-                .stream()
-                .map(Path::toString)
-                .collect(Collectors.joining(File.separator + "*.java "))
-        );
-        // with collectors joining the last line doesn't have the aggregation \*.java
-        String clean = src.toString() + File.separator + "*.java";
-        return clean;
-    }
-
+    @Override
     public String getCommand(String targetURL, String flags, boolean includeLib) {
         StringBuilder command = new StringBuilder("javac -d ");
         // default bin
