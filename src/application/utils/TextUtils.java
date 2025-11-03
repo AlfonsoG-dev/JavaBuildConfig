07347d0eb7436b9b226a3106262dcbb6fa48504b
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-02 22:09:41 -0500

    feat: implement read file lines

--- src/application/utils/TextUtils.java
+++ src/application/utils/TextUtils.java
@@ -1,7 +1,23 @@
 package application.utils;
 
-public class TextUtils {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public record TextUtils() {
     public final static void message(String message) {
         System.out.println(message);
+    }
+    public static String getFileLines(String pathURL) {
+        String lines = "";
+        try(BufferedReader br = new BufferedReader(new FileReader(new File(pathURL)))) {
+            while(br.ready()) {
+                lines += br.readLine() + "\n";
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
     }
 }
