052c9baa248903e10f1e010ef30296a659e10a4a
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-02 22:52:14 -0500

    test: verify the compile command

--- src/application/JavaBuildConfig.java
+++ src/application/JavaBuildConfig.java
@@ -3,14 +3,15 @@ package application;
 import java.io.File;
 
 import application.operation.FileOperation;
+import application.models.CompileModel;
 
 class JavaBuildConfig {
     public static void main(String[] args) {
-        String pathURL = "." + File.separator;
-        FileOperation op = new FileOperation(pathURL);
-        op.populateList(pathURL + "src");
-        op.sourceDirs()
-            .stream()
-            .forEach(System.out::println);
+        String rootURL = "." + File.separator;
+        FileOperation op = new FileOperation(rootURL);
+        op.populateList(rootURL + "src");
+        CompileModel cm = new CompileModel(rootURL, op);
+        String command = cm.getCommand("bin", "", false);
+        System.out.println(command);
     }
 }
