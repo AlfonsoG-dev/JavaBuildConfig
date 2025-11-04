3006d9930166348fd74c69e10f7c99073814dced
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-03 22:08:20 -0500

    feat: add jar command builder

---
+++ src/application/builders/JarBuilder.java
@@ -0,0 +1,74 @@
+package application.builders;
+
+import application.models.CommandModel;
+import application.operation.FileOperation;
+
+import java.io.File;
+
+public record JarBuilder(String root, FileOperation fileOperation) implements CommandModel {
+    @Override
+    public FileOperation getFileOperation() {
+        return fileOperation;
+    }
+    @Override
+    public String getRoot() {
+        return root;
+    }
+    private String jarType(StringBuilder lines, String mainClass) {
+        String executable = "";
+        if(fileOperation.haveManifesto()) {
+            lines.append("m ");
+            executable = "m ";
+        } else if(!mainClass.isEmpty()) {
+            lines.append("e ");
+            executable = "e ";
+        }
+        return executable;
+    }
+    @Override
+    public String getCommand(String targetURL, String flags, boolean includeLib) {
+        StringBuilder lines = new StringBuilder("jar -cf");
+        String mainClass = fileOperation.getMainClass();
+        // jar format
+        String format = jarType(lines, mainClass);
+
+        lines.append(fileOperation.getProjectName() + ".jar ");
+        switch(format) {
+            case "m " -> lines.append("Manifesto.txt");
+            case "e " -> lines.append(mainClass);
+        }
+        lines.append(" -C ");
+        lines.append(targetURL);
+        lines.append(File.separator);
+        lines.append(" .");
+        if(includeLib) {
+            lines.append(" -C ");
+            lines.append("extractionFiles");
+            lines.append(File.separator);
+            lines.append(" .");
+        }
+        return lines.toString();
+    }
+    public String getCommand(String targetURL, String mainClass, String flags, boolean includeLib) {
+        StringBuilder lines = new StringBuilder("jar -cf");
+        // jar format
+        String format = jarType(lines, mainClass);
+
+        lines.append(fileOperation.getProjectName() + ".jar ");
+        switch(format) {
+            case "m " -> lines.append("Manifesto.txt");
+            case "e " -> lines.append(mainClass);
+        }
+        lines.append(" -C ");
+        lines.append(targetURL);
+        lines.append(File.separator);
+        lines.append(" .");
+        if(includeLib) {
+            lines.append(" -C ");
+            lines.append("extractionFiles");
+            lines.append(File.separator);
+            lines.append(" .");
+        }
+        return lines.toString();
+    }
+}
