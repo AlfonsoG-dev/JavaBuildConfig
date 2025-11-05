b732f6aa955fa26eedc0b2ea6f45d9d16ae9e28d
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-05 16:58:00 -0500

    feat: text utils now give ANSI colors

--- src/application/utils/TextUtils.java
+++ src/application/utils/TextUtils.java
@@ -9,6 +9,37 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public record TextUtils() {
+
+    public static record Colors() {
+        /**
+         * color rojo para los valores numéricos
+         */
+        public static final String ANSI_RED = "\u001B[41m";
+        /**
+         * color cyan para el path del archivo
+         */
+        public static final String ANSI_CYAN = "\u001B[46m";
+        /**
+         * quitar color para los resultados no exactos
+         */
+        public static final String ANSI_RESET = "\u001B[0m";
+        /**
+         * color amarillo para los resultados
+         */
+        public static final String ANSI_YELLOW = "\u001B[33m";
+        /**
+         * color amarillo con under-linea 
+         */
+        public static final String YELLOW_UNDERLINED = "\033[4;33m";
+        /**
+         * color con linea baja
+         */
+        public static final String GREEN_UNDERLINED = "\033[4;32m";
+        /**
+         * color para link de file
+         */
+        public static final String RED_UNDERLINED = "\033[4;31m";
+    }
     public final static void message(String message) {
         System.out.println("[Info] " + message);
     }
