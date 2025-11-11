6631580da6290aadcddf62bbc442b21c18bdde5d
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-11 15:17:02 -0500

    build: update build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 $Source="src\application\*.java src\application\builders\*.java src\application\models\*.java src\application\operation\*.java src\application\utils\*.java"
 $Compile="javac -d bin $Source"
-$Run="java -cp 'bin' application.JavaBuildConfig --h"
-Invoke-Expression ($Compile + " && " + $Run)
+$Jar="jar -cfm JavaBuildConfig.jar Manifesto.txt -C bin\ . "
+$Run="java -cp 'bin' application.JavaBuildConfig"
+Invoke-Expression ($Compile + " && " + $Jar + " && " + $Run)
