74bc17a72cf8c25d13b35acac5a0a79d26632d58
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-21 13:05:16 -0500

    fix: close files.walk stream on listing files of path

--- src/application/utils/FileUtils.java
+++ src/application/utils/FileUtils.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.concurrent.Callable;
+import java.util.stream.Stream;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,11 +23,12 @@ public class FileUtils {
     public File getRoot() {
         return new File(root);
     }
-    private void populateList(List<Path> list, String pathURI, int level) throws IOException {
-        if(level > 0) {
-            list.addAll(Files.walk(Paths.get(pathURI), level, FileVisitOption.FOLLOW_LINKS).toList());
-        } else {
-            list.addAll(Files.walk(Paths.get(pathURI), FileVisitOption.FOLLOW_LINKS).toList());
+    private void populateList(List<Path> list, String pathURI, int level) {
+        level = level > 0 ? level:Integer.MAX_VALUE;
+        try(Stream<Path> p = Files.walk(Paths.get(pathURI), level, FileVisitOption.FOLLOW_LINKS)) {
+            list.addAll(p.toList());
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
     public boolean createDirectory(String pathURI) {
@@ -69,29 +71,31 @@ public class FileUtils {
             return "";
         }
     }
+    /**
+     * Copies a directory into a target path.
+     * <p> If the directory to copy is not empty, all the children will be copied.
+     * @param sourceDir - the directory to copy.
+     * @param destinationURI - the target path.
+     */
     public void copyDirectory(Path sourceDir, String destinationURI) {
-        try {
+        try(Stream<Path> p = Files.walk(sourceDir, FileVisitOption.FOLLOW_LINKS)) {
             Path destinationRoot = Paths.get(destinationURI);
-            Files.walk(sourceDir)
-                .forEach(source -> {
-                    try {
-                        Path relative = sourceDir.relativize(source);
-                        Path destination = destinationRoot.resolve(relative);
-    
-                        if (Files.isDirectory(source)) {
-                            Files.createDirectories(destination);
-                        } else {
-                            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
-                            System.out.printf(
-                                "\tCopy { %s%s%s } \n\t\tinto => [ %s%s%s ]%n",
-                                TextUtils.Colors.YELLOW_UNDERLINED, source, TextUtils.Colors.ANSI_RESET,
-                                TextUtils.Colors.GREEN_UNDERLINED, destination, TextUtils.Colors.ANSI_RESET
-                            );
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
+            List<Path> files = p.toList();
+            if(files.isEmpty()) return;
+            for(Path mp: files) {
+                Path relative = sourceDir.relativize(mp);
+                Path destination = destinationRoot.resolve(relative);
+                if (Files.isDirectory(mp)) {
+                    Files.createDirectories(destination);
+                } else {
+                    Files.copy(mp, destination, StandardCopyOption.REPLACE_EXISTING);
+                    System.out.printf(
+                        "\tCopy { %s%s%s } \n\t\tinto => [ %s%s%s ]%n",
+                        TextUtils.Colors.YELLOW_UNDERLINED, mp, TextUtils.Colors.ANSI_RESET,
+                        TextUtils.Colors.GREEN_UNDERLINED, destination, TextUtils.Colors.ANSI_RESET
+                    );
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -99,7 +103,7 @@ public class FileUtils {
     /**
      * Count the items that are type file inside a directory.
      * @param directory the directory to count its files.
-     * @return the number of files inside the given dierectory, if its empty 0.
+     * @return the number of files inside the given directory, if its empty 0.
      */
     public int countFiles(Path directory) {
         int n = 0;
@@ -121,11 +125,7 @@ public class FileUtils {
      */
     public List<Path> listPaths(String pathURI, int level) {
         List<Path> files = new ArrayList<>();
-        try {
-            populateList(files, pathURI, level);
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
+        populateList(files, pathURI, level);
         return files;
     }
     /**
@@ -139,11 +139,7 @@ public class FileUtils {
             @Override
             public List<Path> call() {
                 List<Path> files = new ArrayList<>();
-                try {
-                    populateList(files, pathURI, level);
-                } catch(IOException e) {
-                    e.printStackTrace();
-                }
+                populateList(files, pathURI, level);
                 return files;
             }
         };
